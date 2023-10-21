5267c093a238e64cbb3cc7f474c593ea1f54cff5
lukeding <dingkai9627@outlook.com>
2023-10-22 02:19:34 +0800

    any

--- public/js/config.js
+++ public/js/config.js
@@ -1,1 +1,1 @@
-window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"MYENV":"ENV:\"dev\""}
+window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"MYENV":"ENV:\"prod\""}
