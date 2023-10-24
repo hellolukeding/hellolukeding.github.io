5b2839fde9e4aaf2469fd31250908891771c7f5c
hellolukeding <hellolukeding@users.noreply.github.com>
2023-10-24 13:01:30 +0000

    自动部署 de3b91bdefeed4c442286ee51a2671da2b0ffff7

---
+++ js/config.js
@@ -0,0 +1,1 @@
+window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"MYENV":"ENV:\"prod\""}
