b9292ef6425ec78e2c44eea62d744d4b956cd9fa
lukeding <dingkai9627@outlook.com>
2023-10-29 23:05:29 +0800

    hash

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,18 @@
-import {Navigate, RouteObject, createBrowserRouter} from "react-router-dom";
+import { Navigate, RouteObject, createHashRouter } from "react-router-dom";
 import Welcome from "src/pages/Welcome";
 
-const routers:RouteObject[]=[
+const routers: RouteObject[] = [
   {
-    path:"",
-    element:<Navigate to="/welcome" replace={true} />,
+    path: "",
+    element: <Navigate to="/welcome" replace={true} />,
   },
   {
-    path:"welcome",
-    element:<Welcome />,
-    handle:{
-      title:"欢迎页"
-    }
-  }
-]
+    path: "welcome",
+    element: <Welcome />,
+    handle: {
+      title: "欢迎页",
+    },
+  },
+];
 
-export const crtRouter=createBrowserRouter(routers); 
+export const crtRouter = createHashRouter(routers);
