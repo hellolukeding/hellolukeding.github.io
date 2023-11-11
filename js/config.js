3181ac0240c64f6b5397ab8dc3fbbb8afc1626fa
hellolukeding <dingkai9627@outlook.com>
2023-11-11 18:11:07 +0000

    Deploying to gh-pages from @ hellolukeding/hellolukeding.github.io@7c3e2c8b616bb5bd3ddebe15db38c5e2a7828a51 🚀

--- js/config.js
+++ js/config.js
@@ -1,1 +1,4 @@
-window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"MYENV":"ENV:\"prod\""}
+
+    window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"MYENV":"ENV:\"prod\"","build_time":"11/11/2023, 6:10:46 PM","blogs":{"2023":["【设计模式系列】发布订阅模式"]}};
+    console.log(`%c lukeding builds in ${window.config.build_time}`, "color: #fff; font-size: 12px;border-radius: 2px; background: #2ECC71; padding: 2px 4px;")
+    
