7b2e18b0237a1fc9331cb886ecb33b7ed126e1a3
hellolukeding <dingkai9627@outlook.com>
2023-11-12 09:31:32 +0000

    Deploying to gh-pages from @ hellolukeding/hellolukeding.github.io@2bbaa8d7d744a4eee318b4f34f3d77e7c62eab55 🚀

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 
-    window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"MYENV":"ENV:\"prod\"","build_time":"11/11/2023, 6:10:46 PM","blogs":{"2023":["【设计模式系列】发布订阅模式"]}};
+    window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"MYENV":"ENV:\"prod\"","build_time":"11/12/2023, 9:31:20 AM","blogs":{"2023":["【设计模式系列】发布订阅模式"]}};
     console.log(`%c lukeding builds in ${window.config.build_time}`, "color: #fff; font-size: 12px;border-radius: 2px; background: #2ECC71; padding: 2px 4px;")
     
