5f15346a354a771d1292633ca7148486d0e69900
lukeding <lukeding@lukedingdeMacBook-Pro.local>
2023-12-03 16:18:42 +0800

    feat✨(custom): 发布订阅模式

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 
-    window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"ENV":"dev","build_time":"2023/11/14 01:18:27","blogs":{"2023":["【设计模式系列】发布订阅模式"]}};
+    window.config={"TEST_VAL":"name:\"dev_cookie\"","MONGODB_CONFIG":{"name":"test","type":"mongodb","url":"mongodb://localhost:27017","username":"admin","password":"root","database":"test","entities":"mongo","logging":false,"synchronize":true},"ENV":"dev","build_time":"12/3/2023, 3:03:04 PM","blogs":{"2023":["【设计模式系列】发布订阅模式"]}};
     console.log(`%c lukeding builds in ${window.config.build_time}`, "color: #fff; font-size: 12px;border-radius: 2px; background: #2ECC71; padding: 2px 4px;")
     
