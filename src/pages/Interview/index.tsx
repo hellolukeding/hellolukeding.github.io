88a16a0ca357e4efe1b0610626f0636ecf58b733
lukeding <dingkai9627@outlook.com>
2024-05-26 10:50:34 +0800

    chore: Update build time in config.js, add performance logging in App.tsx, and improve CSS overflow property in Blog page

--- src/pages/Interview/index.tsx
+++ src/pages/Interview/index.tsx
@@ -1,11 +1,16 @@
 import { useGithubIssue } from "src/apis/query/github";
-
+import styles from "./index.module.scss";
 interface InterviewProps {}
 
 const Interview: React.FC<InterviewProps> = (props) => {
   const { data } = useGithubIssue();
   console.log(data);
-  return <></>;
+  return (
+    <section className={styles.main}>
+      <aside className={styles["side"]}></aside>
+      <article className={styles["content"]}></article>
+    </section>
+  );
 };
 
 export default Interview;
