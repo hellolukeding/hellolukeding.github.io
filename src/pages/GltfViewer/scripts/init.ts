14c3a359bdf56d1405c466a5cec9760e595c9519
lukeding <dingkai9627@outlook.com>
2024-06-07 17:37:21 +0800

    chore: Update config.js build time and add responsive 3D viewer functionality

--- src/pages/GltfViewer/scripts/init.ts
+++ src/pages/GltfViewer/scripts/init.ts
@@ -6,16 +6,20 @@ export const init = (container: HTMLDivElement | null) => {
 
   const scene = new THREE.Scene();
 
-  //创建一个长方体几何对象Geometry
-  const geometry = new THREE.BoxGeometry(100, 100, 100);
+  //创建一个长方体几何对象Geometry，根据容器的大小按比例设置长宽高
+  const geometry = new THREE.BoxGeometry(width / 30, width / 30, width / 30);
+
   //创建一个材质对象Material
   const material = new THREE.MeshBasicMaterial({
     color: "#008B8B",
+    opacity: 0.7,
+    transparent: true,
   });
+
   // 两个参数分别为几何体geometry、材质material
   const mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
   //设置网格模型在三维空间中的位置坐标，默认是坐标原点
-  mesh.position.set(0, 10, 0);
+  mesh.position.set(0, 0, 0);
   //网格模型添加到场景中
   scene.add(mesh);
 
